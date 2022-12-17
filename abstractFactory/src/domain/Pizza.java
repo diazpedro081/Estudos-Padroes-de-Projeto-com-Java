563643e0dcad2b429f02c60319c17d0c87f7eb01
diazpedrodev <081diazpedro@gmail.com>
2022-12-17 20:51:56 -0300

    Creational Patterns: Prototype

--- abstractFactory/src/domain/Pizza.java
+++ abstractFactory/src/domain/Pizza.java
@@ -1,5 +1,46 @@
 package domain;
 
-public class Pizza {
+import java.util.ArrayList;
+import java.util.List;
+
+abstract public class Pizza {
     
+    public static enum Sabor {
+        CALABRESA,
+        PEPPERONI,
+        QUEIJO,
+        VEGANA
+    }
+
+    String nome;
+    String massa;
+    String molho;
+    List<String> cobertura = new ArrayList<String>();
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public void preparar() {
+        System.out.println("Preparando " + nome);
+    }
+
+    public void assar() {
+        System.out.println("Assando " + nome);
+    }
+
+    public void cortar() {
+        System.out.println("Cortando " + nome);
+    }
+
+    public void embalar() {
+        System.out.println("EMbalando " + nome);
+    }
+
+    @Override
+    public String toString() {
+        return "A Pizza de " + nome + ", massa=" + massa + ", molho=" + molho + ", cobertura=" + cobertura + "]";
+    }
+
 }
+ 
