2036cfc3c44eca13ec58ad3b7d612ac56d89a634
diazpedrodev <081diazpedro@gmail.com>
2022-12-19 10:39:35 -0300

    Creational Patterns: Abstract Factory

--- abstractFactory/src/domain/PizzariaAguaVerde.java
+++ abstractFactory/src/domain/PizzariaAguaVerde.java
@@ -1,5 +1,20 @@
 package domain;
 
-public class PizzariaAguaVerde {
+import domain.Pizza.Sabor;
+
+public class PizzariaAguaVerde extends Pizzaria{
+
+    @Override
+    Pizza produzirPizza(Sabor sabor) {
+        if (sabor.equals(Pizza.Sabor.QUEIJO)) {
+            return new PizzaDeQueijo();
+        } else if (sabor.equals(Pizza.Sabor.VEGANA)) {
+            return new PizzaVegana();
+        } else if (sabor.equals(Pizza.Sabor.CALABRESA)) {
+            return new PizzaDeCalabrezaBairro();
+        } else if (sabor.equals(Pizza.Sabor.PEPPERONI)) {
+            return new PizzaDePepperoni();
+        } else return null;
+    }
     
 }
