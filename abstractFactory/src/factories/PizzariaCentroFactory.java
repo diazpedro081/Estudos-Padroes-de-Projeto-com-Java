b1a312df553de15e29ead254157d89bae52fe234
diazpedrodev <081diazpedro@gmail.com>
2022-12-19 10:39:17 -0300

    Creational Patterns: Abstract Factory

--- abstractFactory/src/factories/PizzariaCentroFactory.java
+++ abstractFactory/src/factories/PizzariaCentroFactory.java
@@ -1,5 +1,21 @@
 package factories;
 
-public class PizzariaCentroFactory {
+import domain.Pizza;
+import domain.Pizzaria;
+import domain.PizzariaCentro;
+import domain.Pizza.Sabor;
+
+public class PizzariaCentroFactory extends PizzariaFactory{
+
+    private Pizzaria pizzaria;
+
+    public PizzariaCentroFactory() {
+        pizzaria = new PizzariaCentro();
+    }
+
+    @Override
+    public Pizza pedirPizza(Sabor sabor) {
+        return pizzaria.pedirPizza(sabor);
+    }
     
 }
