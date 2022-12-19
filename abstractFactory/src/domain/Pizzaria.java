6f7d8b572154f6dbbb6f36981b6b0ff7c69b7dd0
diazpedrodev <081diazpedro@gmail.com>
2022-12-19 10:39:38 -0300

    Creational Patterns: Abstract Factory

--- abstractFactory/src/domain/Pizzaria.java
+++ abstractFactory/src/domain/Pizzaria.java
@@ -1,5 +1,21 @@
 package domain;
 
-public class Pizzaria {
+public abstract class Pizzaria {
     
+    public static enum Localidade {
+        CENTRO,
+        AGUAVERDE
+    }
+
+    abstract Pizza produzirPizza(Pizza.Sabor sabor);
+
+    public Pizza pedirPizza(Pizza.Sabor sabor) {
+        Pizza pizza = produzirPizza(sabor);
+        System.out.println("Preparando a " + pizza.getNome() + "");
+        pizza.preparar();
+        pizza.assar();
+        pizza.cortar();
+        pizza.embalar();
+        return pizza;
+    }
 }
