a7ad0c527e2a2a791835b2a98d9b11203fae1845
diazpedrodev <081diazpedro@gmail.com>
2022-12-19 10:39:46 -0300

    Creational Patterns: Abstract Factory

--- abstractFactory/src/domain/PizzaDePepperoni.java
+++ abstractFactory/src/domain/PizzaDePepperoni.java
@@ -1,5 +1,13 @@
 package domain;
 
-public class PizzaDePepperoni {
+public class PizzaDePepperoni extends Pizza {
     
+    public PizzaDePepperoni() {
+        nome = "Pizza de Pepperoni";
+        massa = "Massa integral";
+        molho = "Molho Marinara";
+        coberturas.add("Pimentão Picante");
+        coberturas.add("Cebola Picada");
+        coberturas.add("Queijo Parmesão ralado");
+    }
 }
