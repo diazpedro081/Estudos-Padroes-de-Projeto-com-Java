945c90be174f98e1b8d0d6b574d4508d09fba636
diazpedrodev <081diazpedro@gmail.com>
2022-12-19 10:39:41 -0300

    Creational Patterns: Abstract Factory

--- abstractFactory/src/domain/PizzaDeQueijo.java
+++ abstractFactory/src/domain/PizzaDeQueijo.java
@@ -1,5 +1,12 @@
 package domain;
 
-public class PizzaDeQueijo {
+public class PizzaDeQueijo extends Pizza{
     
+    public PizzaDeQueijo() {
+        nome = "Pizza de Queijo";
+        massa = "Massa normal";
+        molho = "Molho Marinara";
+        coberturas.add("Mussarela fresca");
+        coberturas.add("Parmesão");
+    }
 }
