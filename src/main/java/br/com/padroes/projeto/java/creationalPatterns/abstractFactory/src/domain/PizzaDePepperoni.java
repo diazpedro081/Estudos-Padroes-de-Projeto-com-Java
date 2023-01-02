47cbf35defa69ac16a0e48b2a5e91ff43eed6353
diazpedrodev <081diazpedro@gmail.com>
2023-01-02 15:55:49 -0300

    Create PizzaDePepperoni.java

---
+++ src/main/java/br/com/padroes/projeto/java/creationalPatterns/abstractFactory/src/domain/PizzaDePepperoni.java
@@ -0,0 +1,13 @@
+package br.com.padroes.projeto.java.creationalPatterns.abstractFactory.src.domain;
+
+public class PizzaDePepperoni extends Pizza {
+    
+    public PizzaDePepperoni() {
+        nome = "Pizza de Pepperoni";
+        massa = "Massa integral";
+        molho = "Molho Marinara";
+        coberturas.add("Pimentão Picante");
+        coberturas.add("Cebola Picada");
+        coberturas.add("Queijo Parmesão ralado");
+    }
+}
