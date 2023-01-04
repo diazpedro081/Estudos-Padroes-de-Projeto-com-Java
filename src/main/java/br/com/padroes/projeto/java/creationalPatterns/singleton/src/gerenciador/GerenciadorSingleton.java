32edee27fa24865adfce9b6fcc242ad4f0832e40
diazpedrodev <081diazpedro@gmail.com>
2023-01-04 08:56:20 -0300

    Create GerenciadorSingleton.java

---
+++ src/main/java/br/com/padroes/projeto/java/creationalPatterns/singleton/src/gerenciador/GerenciadorSingleton.java
@@ -0,0 +1,42 @@
+package br.com.padroes.projeto.java.creationalPatterns.singleton.src.gerenciador;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.padroes.projeto.java.creationalPatterns.singleton.src.domain.Usuario;
+
+
+
+public class GerenciadorSingleton {
+    
+    private static GerenciadorSingleton instancia;
+
+    private List<Usuario> usuarios;
+
+    private GerenciadorSingleton() {
+
+        usuarios = new ArrayList<Usuario>();
+        usuarios.add(new Usuario("LoginUm", "******"));
+        usuarios.add(new Usuario("LoginDois", "******"));
+    }
+
+    public static GerenciadorSingleton getInstancia() {
+        if(instancia == null) {
+            synchronized (GerenciadorSingleton.class) {
+                if (instancia == null) {
+                    instancia = new GerenciadorSingleton();
+                }
+            }
+        }
+        return instancia;
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+    
+}
